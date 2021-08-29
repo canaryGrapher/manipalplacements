7b9c1697db2ce0555aaf77f529d92882767e058b
Yash Aryan <yasharyan307@outlook.com>
2021-08-30 01:32:24 +0530

    UI upgrade

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,33 +4,21 @@ import "../styles/globals.css";
 import "antd/dist/antd.css";
 import React from "react";
 import Head from "next/head";
+import OneSignal from "react-onesignal";
 
 function MyApp({ Component, pageProps }) {
-  React.useEffect(() => {
-    window.OneSignal = window.OneSignal || [];
-    OneSignal.push(function () {
-      OneSignal.init({
-        appId: "3efdd127-f8a3-42d3-84a4-caa7dd5ed089",
-        safari_web_id:
-          "web.onesignal.auto.5f83a190-684a-4c4c-a875-2ee8aa7bf929",
-        notifyButton: {
-          enable: true,
-        },
-      });
+  const [initialized, setInitialized] = React.useState(false);
+  OneSignal.init({
+    appId: "3efdd127-f8a3-42d3-84a4-caa7dd5ed089",
+    notifyButton: { enable: true },
+  }).then(() => {
+    setInitialized(true);
+    OneSignal.showSlidedownPrompt().then(() => {
+      // do other stuff
     });
-
-    return () => {
-      window.OneSignal = undefined;
-    };
-  }, []);
+  });
   return (
     <React.Fragment>
-      <Head>
-        <script
-          src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
-          async=""
-        ></script>
-      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
